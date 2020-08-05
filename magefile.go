793bd2d4dfc0a85104f610afd814e0050164d3ec
Luke Whrit <lukewhrit@gmail.com>
2020-08-05 04:09:54 -0400

    chore(magefile): add run and format scripts

--- magefile.go
+++ magefile.go
@@ -1,14 +1,30 @@
-//+build mage
+// +build mage
 
 package main
 
 import "github.com/magefile/mage/sh"
 
-// Runs go mod download and then build the binary.
+// Build generates a binary of the project
 func Build() error {
 	if err := sh.Run("go", "mod", "download"); err != nil {
 		return err
 	}
 
 	return sh.Run("go", "build", "--ldflags", "-s -w", "./")
+}
+
+// Format lints and fixes all files in the directory
+func Format() error {
+	return sh.Run("go", "fmt", "./...")
+}
+
+// Run builds a binary and executes it
+func Run() error {
+	err := Build()
+
+	if err != nil {
+		return err
+	}
+
+	return sh.RunV("./pulsar")
 }
