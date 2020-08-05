f14b77d5f82ab94dc7b1bce4e0519268d9d93d61
Luke Whrit <lukewhrit@gmail.com>
2020-08-05 15:09:25 -0400

    feat(routes): add create document endpoint

--- routes.go
+++ routes.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/GreatGodApollo/gospacebin"
 	"github.com/gofiber/fiber"
 	"github.com/spacebin-org/pulsar/config"
 )
 
-func getDocument(id string) (*gospacebin.Document, error) {
-	spacebin := gospacebin.NewClient(config.GetConfig().Instance)
-
-	return spacebin.GetDocument(id)
+type createBody struct {
+	Content string `json:"content" xml:"content" form:"content"`
 }
 
 func registerEndpoints(app *fiber.App) {
+	spacebin := gospacebin.NewClient(config.GetConfig().Instance)
+
 	app.Get("/", func(c *fiber.Ctx) {
 		if err := c.Render("index", fiber.Map{}); err != nil {
 			c.Next(err)
@@ -20,16 +22,16 @@ func registerEndpoints(app *fiber.App) {
 	})
 
 	app.Get("/:id", func(c *fiber.Ctx) {
-		document, err := getDocument(c.Params("id"))
+		document, err := spacebin.GetDocument(c.Params("id"))
 
 		if err != nil {
-			if err := c.Render("document", fiber.Map{
+			if err := c.Render("document", &fiber.Map{
 				"Document": err.Error(),
 			}); err != nil {
 				c.Next(err)
 			}
 		} else {
-			if err := c.Render("document", fiber.Map{
+			if err := c.Render("document", &fiber.Map{
 				"Document": &document.Content,
 				"ID":       &document.ID,
 			}); err != nil {
@@ -39,14 +41,43 @@ func registerEndpoints(app *fiber.App) {
 	})
 
 	app.Get("/:id/raw", func(c *fiber.Ctx) {
-		document, err := getDocument(c.Params("id"))
+		document, err := spacebin.GetDocument(c.Params("id"))
 
 		if err != nil {
-			c.JSON(fiber.Map{
-				"error": err,
+			c.JSON(&fiber.Map{
+				"error": err.Error(),
 			})
 		}
 
 		c.Send(document.Content)
+	})
+
+	app.Post("/upload", func(c *fiber.Ctx) {
+		body := new(createBody)
+
+		if err := c.BodyParser(body); err != nil {
+			c.JSON(&fiber.Map{
+				"error": err.Error(),
+			})
+
+			return
+		}
+
+		if body.Content != "" {
+			resp, err := spacebin.CreateDocument(&gospacebin.CreateDocumentOpts{
+				Content:   body.Content,
+				Extension: "txt",
+			})
+
+			if err != nil {
+				c.JSON(&fiber.Map{
+					"error": err,
+				})
+
+				return
+			}
+
+			c.Redirect(fmt.Sprintf("/%s", resp.ID))
+		}
 	})
 }
