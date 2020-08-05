330b31c0ab7636ab7eaa45323bf627120c85d61b
Luke Whrit <lukewhrit@gmail.com>
2020-08-05 04:59:56 -0400

    feat(routes.go): add get document routes

--- go.mod
+++ go.mod
@@ -3,11 +3,12 @@ module github.com/spacebin-org/pulsar
 go 1.14
 
 require (
+	github.com/GreatGodApollo/gospacebin v0.4.0
 	github.com/gofiber/cors v0.2.2
 	github.com/gofiber/fiber v1.13.3
+	github.com/gofiber/template v1.5.5
 	github.com/klauspost/compress v1.10.10 // indirect
 	github.com/knadh/koanf v0.12.0
 	github.com/magefile/mage v1.10.0
 	golang.org/x/sys v0.0.0-20200805065543-0cf7623e9dbd // indirect
-	github.com/gofiber/template v1.5.5
 )
--- routes.go
+++ routes.go
@@ -1,11 +1,52 @@
 package main
 
-import "github.com/gofiber/fiber"
+import (
+	"github.com/GreatGodApollo/gospacebin"
+	"github.com/gofiber/fiber"
+	"github.com/spacebin-org/pulsar/config"
+)
+
+func getDocument(id string) (*gospacebin.Document, error) {
+	spacebin := gospacebin.NewClient(config.GetConfig().Instance)
+
+	return spacebin.GetDocument(id)
+}
 
 func registerEndpoints(app *fiber.App) {
 	app.Get("/", func(c *fiber.Ctx) {
 		if err := c.Render("index", fiber.Map{}); err != nil {
 			c.Next(err)
 		}
+	})
+
+	app.Get("/:id", func(c *fiber.Ctx) {
+		document, err := getDocument(c.Params("id"))
+
+		if err != nil {
+			if err := c.Render("document", fiber.Map{
+				"Document": err.Error(),
+			}); err != nil {
+				c.Next(err)
+			}
+		} else {
+			if err := c.Render("document", fiber.Map{
+				"Document": &document.Content,
+				"ID":       &document.ID,
+			}); err != nil {
+				c.Next(err)
+			}
+		}
+	})
+
+	app.Get("/:id/raw", func(c *fiber.Ctx) {
+		document, err := getDocument(c.Params("id"))
+
+		if err != nil {
+			c.JSON(fiber.Map{
+				"error": err,
+			})
+		}
+
+		c.Send(document.Content)
 	})
 }
