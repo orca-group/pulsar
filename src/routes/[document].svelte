8103b5b4b9f5274ecf0fe88d353250579fcddbc9
Luke Whrit <lukewhrit@gmail.com>
2020-11-20 16:07:00 -0500

    feat: add frontend layout

---
+++ src/routes/[document].svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  const code = `const fn = () => {
+  console.log('This is a test')
+}`
+</script>
+
+<main>
+  <div id="line-numbers">&gt;</div>
+  <pre><code>{code}</code></pre>
+</main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,26 @@
-<script>
-  import successkid from 'images/successkid.jpg'
+<script lang="ts">
+  import { onMount } from 'svelte'
+
+  onMount(() => {
+      document.querySelector('textarea').addEventListener('keydown', function (e) {
+        if (e.key.toLowerCase() === 'tab') {
+          e.preventDefault()
+
+          const start = this.selectionStart
+          const end = this.selectionEnd
+
+          // set textarea value to: text before caret + tab + text after caret
+          this.value = this.value.substring(0, start) +
+            "\t" + this.value.substring(end)
+
+          // move caret to right position
+          this.selectionStart = this.selectionEnd = start + 1;
+        }
+    })
+  })
 </script>
 
-<style>
-  h1, figure, p {
-    text-align: center;
-    margin: 0 auto;
-  }
-
-  h1 {
-    font-size: 2.8em;
-    text-transform: uppercase;
-    font-weight: 700;
-    margin: 0 0 0.5em 0;
-  }
-
-  figure {
-    margin: 0 0 1em 0;
-  }
-
-  img {
-    width: 100%;
-    max-width: 400px;
-    margin: 0 0 1em 0;
-  }
-
-  p {
-    margin: 1em auto;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
-    }
-  }
-</style>
-
-<h1>Great success!</h1>
-
-<figure>
-  <img alt="Success Kid" src="{successkid}">
-  <figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<main>
+  <div id="line-numbers">&gt;</div>
+  <textarea spellcheck="false"></textarea>
+</main>
