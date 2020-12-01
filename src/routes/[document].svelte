a222d3792cf5ce03a7797d1b36efdb4cd0a38a3c
Luke Whrit <lukewhrit@gmail.com>
2020-11-30 19:14:19 -0500

    feat: implement better error handling

--- src/routes/[document].svelte
+++ src/routes/[document].svelte
@@ -2,11 +2,21 @@
   export async function preload(page) {
     const { document: slug } = page.params
 
+    if (slug.length !== 8) {
+      throw new Error(
+        `Document ID is not long enough; is ${slug.length}, must be 8`
+      )
+    }
+
     const resp: Response = await this.fetch(
       `https://api.spaceb.in/api/v1/documents/${slug}`
     )
-
     const code = await resp.json()
+
+    if (resp.status === 500 || 404 && code.error === 'record not found') {
+      throw new Error(`Could not find document '${slug}'.`)
+    }
+
     const lines = code.payload.content.split('\n')
 
     return { code: code.payload.content, lines }
--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -31,16 +31,18 @@
   }
 </style>
 
-<h1>Error {status}</h1>
+<main>
+  <h1>Error {status}</h1>
 
-<p>{error.message}</p>
+  <p>{error.message}</p>
 
-{#if dev && error.stack}
-  <pre>{error.stack}</pre>
-{/if}
+  {#if dev && error.stack}
+    <pre>{error.stack}</pre>
+  {/if}
 
-<h3>
-  Get help for this error in
-    <a href="https://discord.gg/zsxwgYc">Discord</a> or report it on
-    <a href="https://github.com/spacebin-org/pulsar">Github</a>.
-</h3>
+  <h3>
+    Get help for this error in
+      <a href="https://discord.gg/zsxwgYc">Discord</a> or report it on
+      <a href="https://github.com/spacebin-org/pulsar">Github</a>.
+  </h3>
+</main>
