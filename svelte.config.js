62fe88213cb20be9ffcff09d7ce504d62370c559
Luke <lukewhrit@pm.me>
2022-12-20 16:27:53 -0500

    style: stop using tabs

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,16 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter({ out: 'build' }),
-		env: {
-			publicPrefix: 'PULSAR_',
-		},
-	},
+  kit: {
+    adapter: adapter({ out: 'build' }),
+    env: {
+      publicPrefix: 'PULSAR_',
+    },
+  },
 };
 
 export default config;
